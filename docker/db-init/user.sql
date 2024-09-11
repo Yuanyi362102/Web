58a3d2d1c0d7d1c51744b4ed2ce05bfd7e51a5d2
袁奕 <145186897+Yuanyi362102@users.noreply.github.com>
2024-09-11 18:55:09 +0800

    Update user.sql

--- docker/db-init/user.sql
+++ docker/db-init/user.sql
@@ -12,10 +12,6 @@ INSERT INTO `user` (`id`, `username`, `password`, `pic`) VALUES (62, 'qiuchuang'
 INSERT INTO `user` (`id`, `username`, `password`, `pic`) VALUES (63, 'lihua', '362102', NULL);
 INSERT INTO `user` (`id`, `username`, `password`, `pic`) VALUES (64, '秋窗1', '1234567', 'https://krseoul.imgtbl.com/i/2024/08/19/66c296964f836.jpg');
 INSERT INTO `user` (`id`, `username`, `password`, `pic`) VALUES (65, '秋窗6', '123456', 'https://krseoul.imgtbl.com/i/2024/08/19/66c29697b3c88.jpeg');
-INSERT INTO `user` (`id`, `username`, `password`, `pic`) VALUES (66, '元帅', 'ys20030918', 'https://krseoul.imgtbl.com/i/2024/08/19/66c29697b3c88.jpeg');
-INSERT INTO `user` (`id`, `username`, `password`, `pic`) VALUES (67, '袁帅', '123456', 'https://s2.loli.net/2024/08/19/dIauDHQl4ViGJj1.jpg');
-INSERT INTO `user` (`id`, `username`, `password`, `pic`) VALUES (68, '圆梦', '123456', 'https://s2.loli.net/2024/08/19/G8Rm2kDzjTABvPq.jpg');
-INSERT INTO `user` (`id`, `username`, `password`, `pic`) VALUES (69, '袁meng', 'Aa362102', 'https://s2.loli.net/2024/08/19/dIauDHQl4ViGJj1.jpg');
 INSERT INTO `user` (`id`, `username`, `password`, `pic`) VALUES (70, 'ym', '123456', NULL);
 INSERT INTO `user` (`id`, `username`, `password`, `pic`) VALUES (71, 'ys', '123456', NULL);
 INSERT INTO `user` (`id`, `username`, `password`, `pic`) VALUES (72, 'yy', '123456', 'https://s2.loli.net/2024/08/19/G8Rm2kDzjTABvPq.jpg');
